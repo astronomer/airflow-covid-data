b80625bfa7fc94c027c5090156d6da247aa3b5c3
petedejoy <pete@astronomer.io>
2020-08-27 12:54:56 -0400

    Revert

--- dags/example-dag.py
+++ dags/example-dag.py
@@ -27,7 +27,7 @@ default_args = {
 }
 
 # Using a DAG context manager, you don't have to specify the dag property of each task
-with DAG('example_dag_armis',
+with DAG('example_dag',
          start_date=datetime(2019, 1, 1),
          max_active_runs=3,
          schedule_interval=timedelta(minutes=30),  # https://airflow.apache.org/docs/stable/scheduler.html#dag-runs
